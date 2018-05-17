40c6631626d336f6d3b663eb58ee648280a56817
dgtvillalobos <dgtvillalobos@gmail.com>
2018-05-16 20:35:48 -0400

    productos con CSS

---
+++ Solucion Hostel/CapaWSPresentacion/scrud_producto.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CapaObjeto;
+
+namespace CapaWSPresentacion
+{
+    public partial class scrud_producto : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarGridView();
+            }
+        }
+
+        private void CargarGridView()
+        {
+            WSSoap.WSSHostelClient x = new WSSoap.WSSHostelClient();
+
+            ListaProductos n = new ListaProductos();
+
+            n = x.ProductoRescatar();
+
+            gwListaProductos.DataSource = null;
+            gwListaProductos.DataSource = n.Productos;
+            gwListaProductos.DataBind();
+        }
+
+        protected void btnCrear_Click(object sender, EventArgs e)
+        {
+            WSSoap.WSSHostelClient x = new WSSoap.WSSHostelClient();
+
+            Producto nProducto = new Producto();
+
+            nProducto.Item.Descripcion  = txtDescripcion.Text;
+            nProducto.Item.Precio       = decimal.Parse(txtPrecio.Text);
+            nProducto.Item.Stock        = decimal.Parse(txtStock.Text);
+            nProducto.Item.StockCritico = decimal.Parse(txtStockCritico.Text);
+
+            nProducto = x.ProductoCrear(nProducto);
+
+            txtCodigo.Text = nProducto.Item.Codigo.ToString();
+            txtCodigoRetorno.Text = nProducto.Retorno.Codigo.ToString();
+            txtGlosaRetorno.Text  = nProducto.Retorno.Glosa;
+
+            CargarGridView();
+        }
+
+        protected void btnActualizar_Click(object sender, EventArgs e)
+        {
+            WSSoap.WSSHostelClient x = new WSSoap.WSSHostelClient();
+
+            Producto aProducto = new Producto();
+
+            aProducto.Item.Codigo       = decimal.Parse(txtCodigo.Text);
+            aProducto.Item.Descripcion  = txtDescripcion.Text;
+            aProducto.Item.Precio       = decimal.Parse(txtPrecio.Text);
+            aProducto.Item.Stock        = decimal.Parse(txtStock.Text);
+            aProducto.Item.StockCritico = decimal.Parse(txtStockCritico.Text);
+
+            aProducto = x.ProductoActualizar(aProducto);
+
+            txtCodigoRetorno.Text = aProducto.Retorno.Codigo.ToString();
+            txtGlosaRetorno.Text  = aProducto.Retorno.Glosa;
+
+            CargarGridView();
+        }
+
+        protected void btnEliminar_Click(object sender, EventArgs e)
+        {
+            WSSoap.WSSHostelClient x = new WSSoap.WSSHostelClient();
+
+            Producto eProducto = new Producto();
+
+            eProducto.Item.Codigo = decimal.Parse(txtCodigo.Text);
+
+            eProducto = x.ProductoEliminar(eProducto);
+
+            txtCodigoRetorno.Text = eProducto.Retorno.Codigo.ToString();
+            txtGlosaRetorno.Text = eProducto.Retorno.Glosa;
+
+            CargarGridView();
+        }
+
+    }
+}
