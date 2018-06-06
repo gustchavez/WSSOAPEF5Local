74a868ec39fa0a9604b9452eba0bb4363375ad7a
gustavo chavez <gust.chavez@alumnos.duoc.cl>
2018-06-06 00:18:36 -0400

    modificacion perfilProveedor/Perfil, se modifica pagina maestra en perfilProveedor

--- Solucion Hostel/CapaWSPresentacion/perfilProveedor/Pedidos.aspx
+++ Solucion Hostel/CapaWSPresentacion/perfilProveedor/Pedidos.aspx
@@ -1,23 +1,9 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/perfilProveedor/MasterProveedor.Master" AutoEventWireup="true" CodeBehind="Pedidos.aspx.cs" Inherits="CapaWSPresentacion.perfilProveedor.Pedidos" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
 
-    	<form id="form1" runat="server">
+    <form id="form1" runat="server">
 
-    	<div class="columna1">
-		<div class="perfilCliente">		
-			<div class="datosEmpresa" > <div class="imagen-logo-empresa"></div> </div>
-			<div class="datosEmpresa">  <h3> DATOS ACTUALES </h3> </div>
-			<div class="datosEmpresa" style=""> <b>Razón Social</b>  <br> <label>Nombre Empresa</label> </div>
-			<div class="datosEmpresa">  <b>Rut Empresa</b>  <br>  <label>11111111-1</label> </div>
-			<div class="datosEmpresa">  <b>Giro</b>  <br> <label>Entretención</label> </div>
-			<div class="datosEmpresa">  <b>Correo Electrónico </b> <br> <label>contacto@empresa.cl</label> </div>
-			<div class="datosEmpresa">  <b>Dirección</b>  <br> <label>Almirante Pastene 185</label> </div>
-			<div class="datosEmpresa">  <b>Teléfono</b>  <br> <label>22222222</label> </div>
-		</div>
-	</div>
-	<!--Fin COLUMNA1-->
-
-	<div class="columna2">
+	    <div class="columna2">
 		
 				<h2>Solicitudes de Doña Clarita</h2><br>
 				<table border="0" class="listaFactura">				
--- Solucion Hostel/CapaWSPresentacion/perfilProveedor/perfil.aspx.cs
+++ Solucion Hostel/CapaWSPresentacion/perfilProveedor/perfil.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaObjeto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,10 +12,63 @@ namespace CapaWSPresentacion.perfilProveedor
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                string Perfil = Session["PerfilUsuario"].ToString();
 
+                if (Perfil.Equals("Proveedor") || Perfil.Equals("Administrador"))
+                {
+                    if (!IsPostBack)
+                    {
+                        RescatarDatos();
+                    }
+                }
+                else
+                {
+                    Session["TokenUsuario"] = null;
+                    Response.Redirect("/PaginaComercial/perfilIngreso.aspx");
+                }
+            }
+            catch (Exception ex)
+            {
+                Session["TokenUsuario"] = null;
+                Response.Redirect("/PaginaComercial/perfilIngreso.aspx");
+            }
+        }
+        
+        private void RescatarDatos()
+        {
+            WSSoap.WSSHostelClient x = new WSSoap.WSSHostelClient();
+
+            ContenedorCiudades m = new ContenedorCiudades();
+
+            m = x.CiudadRescatar(Session["TokenUsuario"].ToString());
+
+            txtNombreCiudad.DataSource = null;
+            txtNombreCiudad.DataSource = m.Lista.Where(p => p.CodPais == 56);
+            txtNombreCiudad.DataValueField = "Nombre";
+            txtNombreCiudad.DataTextField = "Nombre";
+            txtNombreCiudad.DataBind();
+
+            ContenedorPerfilUsuarioProveedores n = new ContenedorPerfilUsuarioProveedores();
+
+            n = x.PerfilUsuarioProveedorRescatar(Session["TokenUsuario"].ToString());
+
+            Sesion SesionUsuario = (Sesion)Session["SesionUsuario"];
+
+            var proveedor = n.Lista.Where(p => p.PerfilUsuario.Usuario.Nombre == SesionUsuario.Usuario).SingleOrDefault();
+
+            txtRutEmpresa.Text = proveedor.Proveedor.Rut;
+            txtRazonSocial.Text = proveedor.PerfilUsuario.Empresa.RazonSocial;
+            txtCorreoElectronico.Text = proveedor.PerfilUsuario.Empresa.Email;
+            txtTelefono.Text = proveedor.PerfilUsuario.Empresa.Telefono;
+            txtNombreCiudad.SelectedValue = proveedor.PerfilUsuario.Direccion.NombreCiudad;
+            txtDireccion.Text = proveedor.PerfilUsuario.Direccion.Calle;
+            txtNombreUsuario.Text = proveedor.PerfilUsuario.Usuario.Nombre;
+            txtContraseña.Text = proveedor.PerfilUsuario.Usuario.Clave;
         }
 
-        protected void Button1_Click(object sender, EventArgs e)
+        protected void btnModificar_Click(object sender, EventArgs e)
         {
 
         }
